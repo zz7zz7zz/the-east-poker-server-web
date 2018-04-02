8a78d3cac89dcc91417207c416064aa4101652ad
Long <longhui.yang@inveno.cn>
2018-04-02 20:32:19 +0800

    spring mybatis 整合demo

--- usr/src/main/java/com/poker/user/dao/impl/UserDaoImpl.java
+++ usr/src/main/java/com/poker/user/dao/impl/UserDaoImpl.java
@@ -1,33 +1,33 @@
-package com.poker.user.dao.impl;
-
-import org.springframework.stereotype.Repository;
-
-import com.poker.user.dao.IUserDao;
-import com.poker.user.entity.User;
-
-@Repository
-public class UserDaoImpl implements IUserDao{
-
-	@Override
-	public void addUser(User user) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public User getUserByUuid(String uuid) {
-		User mUser = new User();
-		mUser.setUuid(uuid);
-		mUser.setName("UserDaoImpl getUserByUuid");
-		return mUser;
-	}
-
-	@Override
-	public User getUserUid(long uid) {
-		User mUser = new User();
-		mUser.setUid(uid);
-		mUser.setName("UserDaoImpl getUserUid");
-		return mUser;
-	}
-
-}
+//package com.poker.user.dao.impl;
+//
+//import org.springframework.stereotype.Repository;
+//
+//import com.poker.user.dao.IUserDao;
+//import com.poker.user.entity.User;
+//
+//@Repository
+//public class UserDaoImpl implements IUserDao{
+//
+//	@Override
+//	public void addUser(User user) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public User getUserByUuid(String uuid) {
+//		User mUser = new User();
+//		mUser.setUuid(uuid);
+//		mUser.setName("UserDaoImpl getUserByUuid");
+//		return mUser;
+//	}
+//
+//	@Override
+//	public User getUserUid(long uid) {
+//		User mUser = new User();
+//		mUser.setUid(uid);
+//		mUser.setName("UserDaoImpl getUserUid");
+//		return mUser;
+//	}
+//
+//}
